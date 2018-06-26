d0d6e5083d43cfc0b82f3fe3951c453d8f919b2f
NawaNawa <ltue2129@gmail.com>
2018-06-26 15:41:39 +0800

    完成 news列表更新程式碼

--- js/normal.js
+++ js/normal.js
@@ -1,4 +1,96 @@
+console.log("normal");
 var NawaNawa=NawaNawa||{};
+NawaNawa.Classes=NawaNawa.Classes||{};
+NawaNawa.Classes.firebaseChildsHandler=
+class firebaseChildsHandler
+{
+    constructor(firebaseRef,queryString,titleQueryString,contentQueryString,imageQueryString,orderBy='time')
+    {
+        this.ref=firebaseRef;
+        this.queryString=queryString;
+        this.orderBy=orderBy;
+        this.titleQuery=titleQueryString;
+        this.contentQuery=contentQueryString;
+        this.imageQuery=imageQueryString;
+        this._onValue=data=>{
+            var valsO=data.val();
+            var elements=this.htmlElements;
+            var vals=[];
+            for (var key in valsO)vals.push(valsO[key]);
+            if(this.orderBy)
+                vals.sort((a,b)=>b[this.orderBy]>a[this.orderBy]);
+            for(var i=0;i<elements.length&&i<vals.length;i++)
+            {
+                var element=elements[i],title=element.querySelector(this.titleQuery),content=element.querySelector(this.contentQuery),image=element.querySelector(this.imageQuery);
+                var val=vals[i];
+                if(title)title.innerHTML=val.title;
+                if(content)content.innerHTML=val.content;
+                if(image)image.src=val.image||'./image/home/no_images.png';
+            }
+            this.onValue(data);
+        }
+        if(firebaseRef&&queryString&&titleQueryString&&contentQueryString&&imageQueryString&&orderBy)
+            this.addEventListener();
+    }
+    addEventListener()
+    {
+        if(!this.ref){console.warn("請設定Dbref才會執行handle");return;}
+        if(!this.htmlElements){return;}
+        this.ref.on("value",this._onValue);
+    }
+    onValue(data)
+    {}
+    get htmlElements()
+    {
+        if(!this.queryString){console.warn("請設定篩選HtmlElement樣式否則無法更新");return;}
+        return document.querySelectorAll(this.queryString);
+    }
+
+}
+NawaNawa.Classes.ScrollController=
+class ScrollController
+{
+    constructor(){
+        this.lastTop=0;
+
+        this.addEventListener();
+    }
+    _onScroll(e)
+    {
+        var diff=this.top-this.lastTop;
+        this.lastTop=this.top;
+        this.onScroll(e);
+        if(diff>0)
+            this.onScrollDown(e);
+        else(diff<0)
+            this.onScrollUp(e);
+
+    }
+    onScroll(e)
+    {}
+    onScrollUp(e)
+    {}
+    onScrollDown(e)
+    {}
+    addEventListener()
+    {
+        window.addEventListener("scroll",e=>this._onScroll(e));
+    }
+    updateBody()
+    {
+        var body = document.body; // For Safari
+        var html = document.documentElement;
+        this.body=typeof body.scrollTop!=='undefined'?body:typeof html.scrollTop!=='undefined'?html:undefined;
+    }
+    get top()
+    {return window.pageYOffset}
+    set top(val)
+    {window.pageYOffset=val;}
+    get bottom()
+    {return window.pageYOffset+window.innerHeight;}
+    set bottom(val)
+    {this.top=val-window.innerHeight;}
+}
 import {firebaseConfig} from './firebaseconfig.js';
   firebase.initializeApp(firebaseConfig);
   let initApp = function() {
@@ -44,53 +136,9 @@ import {firebaseConfig} from './firebaseconfig.js';
     firebase.auth().onAuthStateChanged(checkAuth,onerror);
   };
 
+export {NawaNawa} ;
 
 
-NawaNawa.Classes=NawaNawa.Classes||{};
-NawaNawa.Classes.ScrollController=
-class ScrollController
-{
-    constructor(){
-        this.lastTop=0;
-
-        this.addEventListiner();
-    }
-    _onScroll(e)
-    {
-        var diff=this.top-this.lastTop;
-        this.lastTop=this.top;
-        this.onScroll(e);
-        if(diff>0)
-            this.onScrollDown(e);
-        else(diff<0)
-            this.onScrollUp(e);
-
-    }
-    onScroll(e)
-    {}
-    onScrollUp(e)
-    {}
-    onScrollDown(e)
-    {}
-    addEventListiner()
-    {
-        window.addEventListener("scroll",e=>this._onScroll(e));
-    }
-    updateBody()
-    {
-        var body = document.body; // For Safari
-        var html = document.documentElement;
-        this.body=typeof body.scrollTop!=='undefined'?body:typeof html.scrollTop!=='undefined'?html:undefined;
-    }
-    get top()
-    {return window.pageYOffset}
-    set top(val)
-    {window.pageYOffset=val;}
-    get bottom()
-    {return window.pageYOffset+window.innerHeight;}
-    set bottom(val)
-    {this.top=val-window.innerHeight;}
-}
 window.addEventListener("load",()=>
 {
   

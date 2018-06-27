7cf334bc8bddbd41a858ccd16f9bcbc1599c8d5d
NawaNawa <ltue2129@gmail.com>
2018-06-27 21:33:53 +0800

    新增 獎狀動態加載

--- js/index.js
+++ js/index.js
@@ -2,5 +2,8 @@ import './normal.js';
 import {NawaNawa} from  './normal.js';
 window.addEventListener("load",()=>{
     var news=firebase.database().ref("/news").limitToLast(6);
-    var handlerObj=new NawaNawa.Classes.firebaseChildsHandler(news,".news .card",'.card-title','.card-text','img');
+    var nhandlerObj=new NawaNawa.Classes.firebaseChildsHandler(news,".news .card",'.card-title','.card-text','img');
+    var certificate = firebase.database().ref("/certificate").limitToLast(4);
+    var chandlerObj=new NawaNawa.Classes.firebaseChildsHandler(certificate,".medals .card",'.card-title','undefined','img');
+    chandlerObj.addEventListener();
 });
